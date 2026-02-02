fb6a1d12cf706fb36523d26ae1d96153e36baa04
Kirill Neevin <kirillmisha@gmail.com>
2026-02-03 00:14:31 +0300

    upload old project

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.tapsAtSecond = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.acceptButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.indicatorPictire = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.indicatorPictire)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // tapsAtSecond
+            // 
+            resources.ApplyResources(this.tapsAtSecond, "tapsAtSecond");
+            this.tapsAtSecond.Name = "tapsAtSecond";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // acceptButton
+            // 
+            resources.ApplyResources(this.acceptButton, "acceptButton");
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            this.acceptButton.Click += new System.EventHandler(this.acceptButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            resources.ApplyResources(this.AboutToolStripMenuItem, "AboutToolStripMenuItem");
+            this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // indicatorPictire
+            // 
+            this.indicatorPictire.Cursor = System.Windows.Forms.Cursors.Default;
+            this.indicatorPictire.Image = global::Clicker.Properties.Resources.red;
+            resources.ApplyResources(this.indicatorPictire, "indicatorPictire");
+            this.indicatorPictire.Name = "indicatorPictire";
+            this.indicatorPictire.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.indicatorPictire);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tapsAtSecond);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.indicatorPictire)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox tapsAtSecond;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button acceptButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox indicatorPictire;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        private static bool pressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Enabled = true;
+
+            timer.Interval = 1000 / (int)Properties.Settings.Default["TapsAtSecond"];
+            tapsAtSecond.Text = Convert.ToInt32(1 / (timer.Interval / 1000.0)).ToString();
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // убираем хук
+            UnHook();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetHook();
+        }
+
+        #region Timer
+
+        public void CtrlKeyDown()
+        {
+            if (pressed == true)
+            {
+                pressed = false;
+                indicatorPictire.Image = Properties.Resources.red;
+            }
+            else
+            {
+                indicatorPictire.Image = Properties.Resources.green;
+                pressed = true;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (pressed)
+            {
+                pressLeftMouse();
+            }
+        }
+        #endregion
+
+        #region Button hook
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        const int WH_KEYBOARD_LL = 13; // Номер глобального LowLevel-хука на клавиатуру
+        const int WM_KEYDOWN = 0x100; // Сообщения нажатия клавиши
+
+        private LowLevelKeyboardProc _proc;
+
+        private static IntPtr hhook = IntPtr.Zero;
+
+        public void SetHook()
+        {
+            IntPtr hInstance = LoadLibrary("User32");
+            _proc = hookProc;
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+        }
+
+        public static void UnHook()
+        {
+            UnhookWindowsHookEx(hhook);
+        }
+
+        public IntPtr hookProc(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                //////ОБРАБОТКА НАЖАТИЯ
+                if (Marshal.ReadInt32(lParam) == 162)
+                {
+                    CtrlKeyDown();
+                    return (IntPtr)1;
+                }
+                else
+                {
+                    return CallNextHookEx(hhook, code, (int)wParam, lParam);
+                }
+            }
+            else
+                return CallNextHookEx(hhook, code, (int)wParam, lParam);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        static void pressLeftMouse()
+        {
+            mouse_event((uint)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
+            mouse_event((uint)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+        }
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+        #endregion
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int delta = int.Parse(tapsAtSecond.Text);
+                if(delta <= 0)
+                {
+                    throw new Exception("Число должно быть больше нуля.");
+                }
+                timer.Interval = 1000 / delta;
+                Properties.Settings.Default["TapsAtSecond"] = delta;
+                Properties.Settings.Default.Save();
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Этот кликер я написал для своей\nмамы. Ей было сложно тянутся до\nкнопки F12 и я написал новое\nпроложение.", "О программе");
+        }
+    }
+}
